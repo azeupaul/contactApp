8b2f470450a77974409e49ab4794bee5979ac365
azeupaul <azeupaul@gmail.com>
2016-09-29 23:04:16 +0100

    Création de notre méthde save et traitement du formulaire

--- js/app.js
+++ js/app.js
@@ -6,6 +6,20 @@ let app = new Vue({
 			email: "john.doe@test.fr",
 			phone: 699999999
 		}],
-		contact: {}
+		contact: {
+			name: "",
+			email: "",
+			phone: 0
+		}
+	},
+	methods: {
+		save: function(){
+			if(this.contact.name){
+				this.contacts.push(this.contact)
+				this.contact.name = ""
+				this.contact.phone = 0
+				this.contact.email = ""
+			}
+		}
 	}
 })
